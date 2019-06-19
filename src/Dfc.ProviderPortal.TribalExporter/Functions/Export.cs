489c85e5afe1b5dfbc36e1001d230c7c1a290738
Mahmood Akhtar <mahmood233@hotmail.com>
2019-06-19 14:05:29 +0100

    COUR-898 Updated cron expression

--- src/Dfc.ProviderPortal.TribalExporter/Functions/Export.cs
+++ src/Dfc.ProviderPortal.TribalExporter/Functions/Export.cs
@@ -17,7 +17,7 @@ namespace Dfc.ProviderPortal.TribalExporter.Functions
     {
         [FunctionName(nameof(Export))]
         public static async Task Run(
-            [TimerTrigger("46 13 * * *")]TimerInfo myTimer,
+            [TimerTrigger("00 22 * * *")]TimerInfo myTimer,
             ILogger log,
             [Inject] IConfiguration configuration,
             [Inject] IBlobStorageHelper blobStorageHelper,
