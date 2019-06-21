de4b59e3a879dfedab6bb6c0258a159c7bedefaa
Mahmood Akhtar <mahmood233@hotmail.com>
2019-06-21 12:16:07 +0100

    COUR-898 Removing the TEST_IGNORE part of the folder name in abid to get ready to deploy.

--- src/Dfc.ProviderPortal.TribalExporter/Functions/Export.cs
+++ src/Dfc.ProviderPortal.TribalExporter/Functions/Export.cs
@@ -32,7 +32,7 @@ namespace Dfc.ProviderPortal.TribalExporter.Functions
 
             var fileNames = new List<string>();
             var last24HoursAgo = DateTime.Today.AddDays(-1);
-            var providersFileName = $"TEST_IGNORE_{DateTime.Today.ToString("yyyyMMdd")}\\Generated\\Providers_{DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")}.json";
+            var providersFileName = $"{DateTime.Today.ToString("yyyyMMdd")}\\Generated\\Providers_{DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")}.json";
 
             logFile.AppendLine($"24 Hours ago: {last24HoursAgo}");
             logFile.AppendLine($"Provider filename: {providersFileName}");
@@ -140,7 +140,7 @@ namespace Dfc.ProviderPortal.TribalExporter.Functions
                         {
                             logFile.AppendLine($"\t\tHas courses' data for: {mpItem}");
 
-                            var coursesFileName = $"TEST_IGNORE_{DateTime.Today.ToString("yyyyMMdd")}\\Generated\\Courses_for_Provider_{mpItem.Ukprn}_{DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")}.json";
+                            var coursesFileName = $"{DateTime.Today.ToString("yyyyMMdd")}\\Generated\\Courses_for_Provider_{mpItem.Ukprn}_{DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")}.json";
 
                             logFile.AppendLine($"\t\tGot reference to block bolb containers for file: {coursesFileName}");
 
@@ -171,7 +171,7 @@ namespace Dfc.ProviderPortal.TribalExporter.Functions
                         {
                             logFile.AppendLine($"\t\tHas venues' data for: {mpItem}");
 
-                            var venuesFileName = $"TEST_IGNORE_{DateTime.Today.ToString("yyyyMMdd")}\\Generated\\Venues_for_Provider_{mpItem.Ukprn}_{DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")}.json";
+                            var venuesFileName = $"{DateTime.Today.ToString("yyyyMMdd")}\\Generated\\Venues_for_Provider_{mpItem.Ukprn}_{DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")}.json";
 
                             logFile.AppendLine($"\t\tGot reference to block bolb containers for file: {venuesFileName}");
 
@@ -210,7 +210,7 @@ namespace Dfc.ProviderPortal.TribalExporter.Functions
             finally
             {
                 logFile.AppendLine($"Ending {nameof(Export)} at {DateTime.Now}");
-                var logFileName = $"TEST_IGNORE_{DateTime.Today.ToString("yyyyMMdd")}\\Generated\\Log_{DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")}.txt";
+                var logFileName = $"{DateTime.Today.ToString("yyyyMMdd")}\\Generated\\Log_{DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")}.txt";
                 var logFileNameBolb = containerExporter.GetBlockBlobReference(logFileName);
                 await logFileNameBolb.UploadTextAsync(logFile.ToString());
             }
