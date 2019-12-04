a250ccdafced902b70fe9ecde5ab3bc8815e6a8f
Steve O'Hara <github@soharadesign.com>
2019-12-04 13:43:19 +0000

    [COUR-2277] added shortcut to return default DateTime if empty string is passed to DateHelper

---
+++ src/Dfc.ProviderPortal.TribalExporter/Helpers/DateHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dfc.ProviderPortal.TribalExporter.Helpers
+{
+    public static class DateHelper
+    {
+        public static DateTime ParseUkDateOrDefault(this string input, DateTime defaultDate)
+        {
+            var timeFormat = new CultureInfo("en-GB");
+            if (string.IsNullOrEmpty(input)) return defaultDate;
+            try
+            {
+                return Convert.ToDateTime(input, timeFormat);
+            }
+            catch
+            {
+                return defaultDate;
+            }
+        }
+
+        public static double DaysBetween(this DateTime startDate, DateTime endDate)
+        {
+            var days = (endDate - startDate).TotalDays;
+            return Math.Ceiling(days);
+        }
+    }
+}
