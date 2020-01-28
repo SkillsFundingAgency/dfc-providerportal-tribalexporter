ec7c2e50e493f70ff9a5c5b046c19c821f9b95b8
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2020-01-28 13:15:59 +0000

    added http

--- src/Dfc.ProviderPortal.TribalExporter/WebJobsExtensionStartup.cs
+++ src/Dfc.ProviderPortal.TribalExporter/WebJobsExtensionStartup.cs
@@ -35,7 +35,7 @@ using Dfc.CourseDirectory.Services.Interfaces.OnspdService;
 using Dfc.CourseDirectory.Services.OnspdService;
 using Microsoft.Extensions.Logging.Configuration;
 using ApprenticeshipServiceSettings = Dfc.ProviderPortal.TribalExporter.Settings.ApprenticeshipServiceSettings;
-using Microsoft.Azure.WebJobs.Extensions.Httpusing Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting;
 
 [assembly: WebJobsStartup(typeof(WebJobsExtensionStartup), "Web Jobs Extension Startup")]
 
