4cc9519d547fc6c3733a8152e25dd55aca65a720
Javeed Dutia <javeed.dutia@esynergy-solutions.co.uk>
2020-02-13 10:24:33 +0000

    COUR-2328
    https://skillsfundingagency.atlassian.net/browse/COUR-2328

---
+++ src/Dfc.ProviderPortal.TribalExporter/Functions/ValidateCourse.cs
@@ -0,0 +1,109 @@
+﻿using CsvHelper;
+using Dfc.CourseDirectory.Models.Models.Apprenticeships;
+using Dfc.CourseDirectory.Models.Models.Providers;
+using Dfc.ProviderPortal.Packages.AzureFunctions.DependencyInjection;
+using Dfc.ProviderPortal.TribalExporter.Interfaces;
+using Dfc.ProviderPortal.TribalExporter.Models.Tribal;
+using Dfc.ProviderPortal.TribalExporter.Validators;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using UkrlpService;
+
+namespace Dfc.ProviderPortal.TribalExporter.Functions
+{
+    public static class ValidateCourse
+    {
+        [FunctionName(nameof(ValidateCourse))]
+        [NoAutomaticTrigger]
+        public static async Task Run(
+                    string input,  // Work around https://github.com/Azure/azure-functions-vs-build-sdk/issues/168
+                    ILogger log,
+                    [Inject] IConfigurationRoot configuration,
+                    [Inject] ICourseCollectionService courseCollectionService,
+                    [Inject] ICosmosDbHelper cosmosDbHelper,
+                    [Inject] IBlobStorageHelper blobhelper,
+                    [Inject] IUkrlpApiService ukrlpApiService
+                    )
+        {
+            const string apprenticeshipAppName = "Validate.Course";
+
+            var apprenticeshipValidationFileName = $"{apprenticeshipAppName}-{DateTime.Now.ToString("dd-MM-yy HHmm")}";
+            var blobContainer = blobhelper.GetBlobContainer(configuration["BlobStorageSettings:Container"]);
+            var validator = new CourseValidator();
+
+            List<CourseValidationResult> validationEResult = new List<CourseValidationResult>();
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+      
+            var resultData = await courseCollectionService.GetAllCoursesAsync();
+
+            foreach(var item in resultData)
+            {
+                //item.ContactEmail = "testing";
+                //item.Url = "testing";
+                //item.ContactWebsite = "testing";
+                //item.ContactTelephone = "101101abc";
+                //item.ApprenticeshipTitle = item.ApprenticeshipTitle + " @";
+
+                var validationResult = validator.Validate(item);
+
+                if(!validationResult.IsValid)
+                {
+                    foreach(var error in validationResult.Errors)
+                    {
+                        validationEResult.Add(new CourseValidationResult()
+                        {
+                            CourseId = item.CourseId,
+                            ProviderUkprn = item.ProviderUKPRN,
+                            FieldName = error.PropertyName,
+                            ErrorMessage = error.ErrorMessage
+                        });
+                    }
+                }
+            }
+
+            var resultsObjBytes = GetResultAsByteArray(validationEResult);
+            await WriteResultsToBlobStorage(resultsObjBytes);
+
+            stopWatch.Stop();
+
+            byte[] GetResultAsByteArray(IList<CourseValidationResult> message)
+            {
+                using (var memoryStream = new System.IO.MemoryStream())
+                {
+                    using (var streamWriter = new System.IO.StreamWriter(memoryStream))
+                    using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                    {
+                        csvWriter.WriteRecords<CourseValidationResult>(message);
+                    }
+                    return memoryStream.ToArray();
+                }
+            }
+
+            async Task WriteResultsToBlobStorage(byte[] data)
+            {
+                await blobhelper.UploadFile(blobContainer, apprenticeshipValidationFileName, data);
+            }
+        }
+    }
+}
+
+public class CourseValidationResult
+{
+    public int? CourseId { get; set; }
+    public int ProviderUkprn { get; set; }
+    public string FieldName { get; set; }
+    public string ErrorMessage { get; set; }
+}
---
+++ src/Dfc.ProviderPortal.TribalExporter/Validators/CourseValidator.cs
@@ -0,0 +1,68 @@
+﻿using Dfc.CourseDirectory.Models.Models.Apprenticeships;
+using Dfc.CourseDirectory.Models.Models.Courses;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dfc.ProviderPortal.TribalExporter.Validators
+{
+    public class CourseValidator : AbstractValidator<Course>
+    {
+        public CourseValidator()
+        {
+
+            RuleFor(a => a.QualificationCourseTitle)
+                            .Must(b => ValidationHelper.HasNoSpecialCharacters(b))
+                            .WithMessage($"QualificationCourseTitle contains invalid characters.")
+                            .When(a => !string.IsNullOrWhiteSpace(a.QualificationCourseTitle));
+            RuleFor(a => a.CourseDescription)
+                            .Must(b => ValidationHelper.HasNoSpecialCharacters(b))
+                            .WithMessage($"CourseDescription contains invalid characters.")
+                            .When(a => !string.IsNullOrWhiteSpace(a.CourseDescription));
+            RuleFor(a => a.EntryRequirements)
+                            .Must(b => ValidationHelper.HasNoSpecialCharacters(b))
+                            .WithMessage($"EntryRequirements contains invalid characters.")
+                            .When(a => !string.IsNullOrWhiteSpace(a.EntryRequirements));
+            RuleFor(a => a.WhatYoullNeed)
+                            .Must(b => ValidationHelper.HasNoSpecialCharacters(b))
+                            .WithMessage($"WhatYoullNeed contains invalid characters.")
+                            .When(a => !string.IsNullOrWhiteSpace(a.WhatYoullNeed));
+            RuleFor(a => a.HowYoullBeAssessed)
+                            .Must(b => ValidationHelper.HasNoSpecialCharacters(b))
+                            .WithMessage($"HowYoullBeAssessed contains invalid characters.")
+                            .When(a => !string.IsNullOrWhiteSpace(a.HowYoullBeAssessed));
+            RuleFor(a => a.HowYoullBeAssessed)
+                            .Must(b => ValidationHelper.HasNoSpecialCharacters(b))
+                            .WithMessage($"HowYoullBeAssessed contains invalid characters.")
+                            .When(a => !string.IsNullOrWhiteSpace(a.HowYoullBeAssessed));
+
+            RuleForEach(a => a.CourseRuns).SetValidator(new CourseRunValidator());
+        }
+    }
+
+    public class CourseRunValidator : AbstractValidator<CourseRun>
+    {
+        public CourseRunValidator()
+        {
+            RuleFor(r => r.CourseName)
+                            .Must(b => ValidationHelper.HasNoSpecialCharacters(b))
+                            .WithMessage($"CourseName contains invalid characters.")
+                            .When(r => !string.IsNullOrWhiteSpace(r.CourseName));
+
+            RuleFor(r => r.ProviderCourseID)
+                            .Matches(ValidationHelper.UrlRegex)
+                            .When(r => !string.IsNullOrWhiteSpace(r.ProviderCourseID));
+
+            RuleFor(r => r.CourseURL)
+                .Matches(ValidationHelper.UrlRegex)
+                .When(r => !string.IsNullOrWhiteSpace(r.CourseURL));
+
+            RuleFor(r => r.CostDescription)
+                .Matches(ValidationHelper.UrlRegex)
+                .When(r => !string.IsNullOrWhiteSpace(r.CostDescription));
+        }
+    }
+}
+
