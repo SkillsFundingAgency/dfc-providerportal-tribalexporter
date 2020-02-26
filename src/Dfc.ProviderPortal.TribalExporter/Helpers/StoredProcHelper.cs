8e879c97baaceabd73cb0045886c692531d2aa8d
James Gunn <james@gunn.io>
2020-02-26 13:54:15 +0000

    Add function to archive courses for providers in whitelist

---
+++ src/Dfc.ProviderPortal.TribalExporter/Helpers/StoredProcHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace Dfc.ProviderPortal.TribalExporter.Helpers
+{
+    public static class StoredProcHelper
+    {
+        public static async Task UpdateArchiveCoursesForProvider(DocumentClient documentClient)
+        {
+            var databaseId = "providerportal";
+            var collectionId = "courses";
+            var spId = "ArchiveCoursesForProvider";
+
+            var sprocResourceName = "Dfc.ProviderPortal.TribalExporter.ArchiveCoursesForProvider.js";
+
+            using (var stream = typeof(StoredProcHelper).Assembly.GetManifestResourceStream(sprocResourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                var body = reader.ReadToEnd();
+
+                var sp = new StoredProcedure()
+                {
+                    Id = spId,
+                    Body = body
+                };
+
+                var collectionLink = UriFactory.CreateDocumentCollectionUri(databaseId, collectionId);
+                try
+                {
+                    await documentClient.CreateStoredProcedureAsync(collectionLink, sp);
+                }
+                catch (DocumentClientException ex) when (ex.StatusCode == System.Net.HttpStatusCode.Conflict)
+                {
+                    // Already exists - update it
+                    var spLink = UriFactory.CreateStoredProcedureUri(databaseId, collectionId, spId);
+                    await documentClient.ReplaceStoredProcedureAsync(spLink, sp);
+                }
+            }
+        }
+    }
+}
