21981230d6036c8b0d1ec03022c2a74fd53fba66
James Gunn <james@gunn.io>
2020-02-27 16:18:03 +0000

    Don't use sproc - it explodes when lots of docs are updated

--- src/Dfc.ProviderPortal.TribalExporter/Functions/ArchiveCourses.cs
+++ src/Dfc.ProviderPortal.TribalExporter/Functions/ArchiveCourses.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using Dfc.CourseDirectory.Models.Models.Courses;
 using Dfc.ProviderPortal.Packages.AzureFunctions.DependencyInjection;
 using Dfc.ProviderPortal.TribalExporter.Interfaces;
 using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -27,22 +30,51 @@ namespace Dfc.ProviderPortal.TribalExporter.Functions
             var coursesCollectionId = "courses";
             var documentClient = cosmosDbHelper.GetClient();
 
-            var logger = loggerFactory.CreateLogger(typeof(ArchiveCourses));
+            var coursesCollectionUri = UriFactory.CreateDocumentCollectionUri(databaseId, coursesCollectionId);
 
-            var sprocLink = UriFactory.CreateStoredProcedureUri(databaseId, coursesCollectionId, "ArchiveCoursesForProvider");
+            var logger = loggerFactory.CreateLogger(typeof(ArchiveCourses));
 
             var whitelist = await GetProviderWhiteList();
 
             foreach (var ukprn in whitelist)
             {
-                var response = await documentClient.ExecuteStoredProcedureAsync<int>(
-                    sprocLink,
-                    new RequestOptions()
+                var updated = 0;
+
+                string continuation = null;
+                do
+                {
+                    var feedOptions = new FeedOptions()
                     {
+                        RequestContinuation = continuation,
                         PartitionKey = new Microsoft.Azure.Documents.PartitionKey(ukprn)
-                    },
-                    ukprn);
-                var updated = response.Response;
+                    };
+
+                    var queryResponse = await documentClient.CreateDocumentQuery<Course>(coursesCollectionUri, feedOptions)
+                        .Where(p => p.ProviderUKPRN == ukprn && p.CourseStatus != CourseDirectory.Models.Enums.RecordStatus.Archived)
+                        .AsDocumentQuery()
+                        .ExecuteNextAsync<Course>();
+
+                    foreach (var doc in queryResponse)
+                    {
+                        foreach (var run in doc.CourseRuns)
+                        {
+                            run.RecordStatus = CourseDirectory.Models.Enums.RecordStatus.Archived;
+                        }
+
+                        var documentLink = UriFactory.CreateDocumentUri(databaseId, coursesCollectionId, doc.id.ToString());
+
+                        await documentClient.ReplaceDocumentAsync(documentLink, doc, new RequestOptions()
+                        {
+                            PartitionKey = new Microsoft.Azure.Documents.PartitionKey(ukprn)
+                        });
+
+                        updated++;
+                    }
+
+                    continuation = queryResponse.ResponseContinuation;
+                }
+                while (continuation != null);
+
                 logger.LogInformation($"Archived {updated} courses for {ukprn}");
             }
 
