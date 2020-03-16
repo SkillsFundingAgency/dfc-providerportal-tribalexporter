70c373b02a809cb7662914106617e5270e264fbe
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2020-03-16 07:01:18 +0000

    Archive old venues (#183)
    * code.
    
    * simple tests.
    
    * added hashcode support.
    
    * typo
    
    * providerid
    
    * added tolower()

---
+++ src/Dfc.CourseDirectory.Models/Helpers/VenueComparer.cs
@@ -0,0 +1,27 @@
+﻿using Dfc.CourseDirectory.Models.Models.Venues;
+using System;
+using System.Collections.Generic;
+
+namespace Dfc.CourseDirectory.Models.Helpers
+{
+    public class VenueEqualityComparer : IEqualityComparer<Venue>
+    {
+        public bool Equals(Venue x, Venue y)
+        {
+            if ((x.VenueName?.ToLower() == y.VenueName?.ToLower() && 
+                (x.PostCode?.ToLower() == y.PostCode?.ToLower()) &&
+                (x.Address1?.ToLower() == y.Address1?.ToLower()) &&
+                (x.ProviderID == y.ProviderID)) ||
+                (x.ID == y.ID)) 
+                    return true;
+
+            //not the same
+            return false;
+        }
+
+        public int GetHashCode(Venue obj)
+        {
+            return HashCode.Combine(obj.VenueName?.ToLower(), obj.ID, obj.Address1?.ToLower(), obj.PostCode?.ToLower(), obj.ProviderID);
+        }
+    }
+}
---
+++ tests/Dfc.ProviderPortal.TribalExporter.Tests/Helpers/VenueComparerTests.cs
@@ -0,0 +1,117 @@
+﻿using Dfc.CourseDirectory.Models.Models.Venues;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+using Dfc.CourseDirectory.Models.Helpers;
+
+namespace Dfc.ProviderPortal.TribalExporter.Tests.Helpers
+{
+
+    public class VenueComparerTests
+    {
+        [Fact]
+        public void can_group_by_single_venue()
+        {
+            var lst = new List<Venue>() {
+            new Venue()
+            {
+                ID = Guid.Empty.ToString(),
+                Address1 = "address1",
+                VenueName = "venue1",
+                PostCode = "postcode1"
+            } };
+
+            var comp = new VenueEqualityComparer();
+            var uniqueGroups = lst.GroupBy(x => x, comp);
+            Assert.True(uniqueGroups.Count() == 1, $"Expected 1 Groups but got {uniqueGroups.Count()}");
+        }
+
+
+        [Fact]
+        public void that_venues_are_grouped_by_id()
+        {
+            var lst = new List<Venue>() {
+            new Venue()
+            {
+                ID = Guid.Empty.ToString(),
+                Address1 = "address1",
+                VenueName = "venue1",
+                PostCode = "postcode1"
+            },
+            new Venue()
+            {
+                ID = Guid.Empty.ToString(),
+                Address1 = "address2",
+                VenueName = "venue2",
+                PostCode = "postcode2"
+            } };
+
+            var comp = new VenueEqualityComparer();
+            var uniqueGroups = lst.GroupBy(x => x, comp);
+            Assert.True(uniqueGroups.Count() == 1, $"Expected 1 Groups but got {uniqueGroups.Count()}");
+        }
+
+        [Fact]
+        public void that_there_are_no_duplicates_with_distinct_venues()
+        {
+            var lst = new List<Venue>() {
+            new Venue()
+            {
+                ID = Guid.Empty.ToString(),
+                Address1 = "address1",
+                VenueName = "venue1",
+                PostCode = "postcode1"
+            },
+            new Venue()
+            {
+                ID = Guid.NewGuid().ToString(),
+                Address1 = "address2",
+                VenueName = "venue2",
+                PostCode = "postcode2"
+            },
+            new Venue()
+            {
+                ID = Guid.NewGuid().ToString(),
+                Address1 = "address3",
+                VenueName = "venue3",
+                PostCode = "postcode3"
+            }};
+
+            var comp = new VenueEqualityComparer();
+            var uniqueGroups = lst.GroupBy(x => x, comp);
+            Assert.True(uniqueGroups.Count() == 3, $"Expected 3 Groups but got {uniqueGroups.Count()}");
+        }
+
+        [Fact]
+        public void that_there_are_two_distinct_addresses()
+        {
+            var lst = new List<Venue>() { 
+            new Venue()
+            {
+                ID = Guid.Empty.ToString(),
+                Address1 = "address1",
+                VenueName = "test",
+                PostCode = "test"
+            }, 
+            new Venue()
+            {
+                ID = Guid.Empty.ToString(),
+                Address1 = "test",
+                VenueName = "test",
+                PostCode = "test"
+            },
+            new Venue()
+            {
+                ID = Guid.NewGuid().ToString(),
+                Address1 = "test",
+                VenueName = "test",
+                PostCode = "test"
+            }};
+
+            var comp = new VenueEqualityComparer();
+            var uniqueGroups = lst.GroupBy(x => x,  comp);
+            Assert.True(uniqueGroups.Count() == 2, $"Expected 2 Groups but got {uniqueGroups.Count()}");
+        }
+    }
+}
