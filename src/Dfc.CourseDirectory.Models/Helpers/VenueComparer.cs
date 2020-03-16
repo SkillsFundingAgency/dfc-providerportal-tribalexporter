392c474f66f8fa7c3c5588fc5c50aee6958b0e38
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2020-03-16 07:22:31 +0000

    removed id from grouping.

--- src/Dfc.CourseDirectory.Models/Helpers/VenueComparer.cs
+++ src/Dfc.CourseDirectory.Models/Helpers/VenueComparer.cs
@@ -8,12 +8,10 @@ namespace Dfc.CourseDirectory.Models.Helpers
     {
         public bool Equals(Venue x, Venue y)
         {
-            if ((x.VenueName?.ToLower() == y.VenueName?.ToLower() && 
+            if ((x.VenueName?.ToLower() == y.VenueName?.ToLower() &&
                 (x.PostCode?.ToLower() == y.PostCode?.ToLower()) &&
-                (x.Address1?.ToLower() == y.Address1?.ToLower()) &&
-                (x.ProviderID == y.ProviderID)) ||
-                (x.ID == y.ID)) 
-                    return true;
+                (x.Address1?.ToLower() == y.Address1?.ToLower())))
+                return true;
 
             //not the same
             return false;
@@ -21,7 +19,7 @@ namespace Dfc.CourseDirectory.Models.Helpers
 
         public int GetHashCode(Venue obj)
         {
-            return HashCode.Combine(obj.VenueName?.ToLower(), obj.ID, obj.Address1?.ToLower(), obj.PostCode?.ToLower(), obj.ProviderID);
+            return HashCode.Combine(obj.VenueName?.ToLower(), obj.ProviderID, obj.Address1?.ToLower(), obj.PostCode?.ToLower());
         }
     }
 }
