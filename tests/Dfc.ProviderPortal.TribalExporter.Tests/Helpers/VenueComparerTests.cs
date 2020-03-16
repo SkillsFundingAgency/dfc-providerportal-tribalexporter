0e7d62cc46c74c1e26e7df646877d170c7655914
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2020-03-16 08:12:28 +0000

    removed id from grouping. (#184)

--- tests/Dfc.ProviderPortal.TribalExporter.Tests/Helpers/VenueComparerTests.cs
+++ tests/Dfc.ProviderPortal.TribalExporter.Tests/Helpers/VenueComparerTests.cs
@@ -27,31 +27,6 @@ namespace Dfc.ProviderPortal.TribalExporter.Tests.Helpers
             Assert.True(uniqueGroups.Count() == 1, $"Expected 1 Groups but got {uniqueGroups.Count()}");
         }
 
-
-        [Fact]
-        public void that_venues_are_grouped_by_id()
-        {
-            var lst = new List<Venue>() {
-            new Venue()
-            {
-                ID = Guid.Empty.ToString(),
-                Address1 = "address1",
-                VenueName = "venue1",
-                PostCode = "postcode1"
-            },
-            new Venue()
-            {
-                ID = Guid.Empty.ToString(),
-                Address1 = "address2",
-                VenueName = "venue2",
-                PostCode = "postcode2"
-            } };
-
-            var comp = new VenueEqualityComparer();
-            var uniqueGroups = lst.GroupBy(x => x, comp);
-            Assert.True(uniqueGroups.Count() == 1, $"Expected 1 Groups but got {uniqueGroups.Count()}");
-        }
-
         [Fact]
         public void that_there_are_no_duplicates_with_distinct_venues()
         {
@@ -86,14 +61,14 @@ namespace Dfc.ProviderPortal.TribalExporter.Tests.Helpers
         [Fact]
         public void that_there_are_two_distinct_addresses()
         {
-            var lst = new List<Venue>() { 
+            var lst = new List<Venue>() {
             new Venue()
             {
                 ID = Guid.Empty.ToString(),
                 Address1 = "address1",
                 VenueName = "test",
                 PostCode = "test"
-            }, 
+            },
             new Venue()
             {
                 ID = Guid.Empty.ToString(),
@@ -110,7 +85,7 @@ namespace Dfc.ProviderPortal.TribalExporter.Tests.Helpers
             }};
 
             var comp = new VenueEqualityComparer();
-            var uniqueGroups = lst.GroupBy(x => x,  comp);
+            var uniqueGroups = lst.GroupBy(x => x, comp);
             Assert.True(uniqueGroups.Count() == 2, $"Expected 2 Groups but got {uniqueGroups.Count()}");
         }
     }
