5c473824bdaa9f992eb4f062ff553bbcb742d5e1
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2020-03-18 16:59:21 +0000

    changes.

--- src/Dfc.ProviderPortal.TribalExporter/Functions/QAStatusMigrator.cs
+++ src/Dfc.ProviderPortal.TribalExporter/Functions/QAStatusMigrator.cs
@@ -10,6 +10,13 @@ using System.Threading.Tasks;
 using Dapper;
 using System.Data.SqlClient;
 using System.Linq;
+using Dfc.CourseDirectory.Models.Models.Courses;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Dfc.CourseDirectory.Models.Models.Providers;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
 
 namespace Dfc.ProviderPortal.TribalExporter.Functions
 {
@@ -38,28 +45,132 @@ namespace Dfc.ProviderPortal.TribalExporter.Functions
             var tribalConnectionString = configuration.GetConnectionString("TribalRestore");
             var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
             var qaStatuses = new List<ProviderQAStatus>();
+            var cosmosDbClient = cosmosDbHelper.GetClient();
+            var databaseId = configuration["CosmosDbSettings:DatabaseId"];
+            var coursesCollectionId = "ukrlp";
+            var logFileName = $"QAStatusMigrator-{DateTime.Now.ToString("dd-MM-yy HHmm")}";
+            var blobContainer = configuration["BlobStorageSettings:Container"];
+            var whitelistFileName = "ProviderWhiteList.txt";
+            var whitelist = await GetProviderWhiteList();
 
             using (var sqlConnection = new SqlConnection(tribalConnectionString))
             {
-                var sql = @"
-SELECT PassedOverallQAChecks,Ukprn
-FROM [Provider]";
-                qaStatuses = sqlConnection.Query<ProviderQAStatus>(sql).ToList() ;
+                var sql = @"SELECT DISTINCT PassedOverallQAChecks,Ukprn
+                            FROM [Provider]";
+                qaStatuses = sqlConnection.Query<ProviderQAStatus>(sql).ToList();
             }
 
-            //look up id in provider
-
-            foreach(var s in qaStatuses)
+            try
             {
-                Console.WriteLine($"{s.UKPRN} - {s.PassedOverallQAChecks}");
+
+                using (var logStream = new MemoryStream())
+                using (var logStreamWriter = new StreamWriter(logStream))
+                using (var logCsvWriter = new CsvWriter(logStreamWriter, CultureInfo.InvariantCulture))
+                {
+
+
+                    // archived venues
+                    logCsvWriter.WriteField("UKPRN");
+                    logCsvWriter.WriteField("CosmosID");
+                    logCsvWriter.WriteField("PassedOverallQAChecks");
+                    logCsvWriter.NextRecord();
+
+                    using (var sqlConnection = new SqlConnection(defaultConnectionString))
+                    {
+                        foreach (var s in qaStatuses)
+                        {
+                            if (!whitelist.Contains(s.UKPRN))
+                            {
+                                continue;
+                            }
+
+                            var provider = await GetExistingCourse(s.UKPRN.ToString(), cosmosDbClient);
+                            if (s.PassedOverallQAChecks)
+                            {
+                                var sql = @"IF NOT EXISTS (SELECT 1 FROM [Pttcd].[Providers] WHERE ProviderID = @ID) 
+                                        BEGIN
+                                            INSERT INTO [Pttcd].[Providers] (ProviderId,ApprenticeshipQAStatus) SELECT @ID,@Status
+                                        END";
+                                var result = sqlConnection.Execute(sql, new
+                                {
+                                    ID = provider.id,
+                                    Status = 16
+                                });
+                            }
+
+                            // archived venues
+                            logCsvWriter.WriteField(s.UKPRN);
+                            logCsvWriter.WriteField(provider?.id);
+                            logCsvWriter.WriteField(s.PassedOverallQAChecks);
+                            logCsvWriter.NextRecord();
+
+                        }
+                    }
+
+                    // Upload log CSV to blob storage
+                    {
+                        logStreamWriter.Flush();
+
+                        logStream.Seek(0L, SeekOrigin.Begin);
+
+                        var blob = blobHelper.GetBlobContainer(blobContainer).GetBlockBlobReference(logFileName);
+                        await blob.UploadFromStreamAsync(logStream);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
 
+            async Task<ISet<int>> GetProviderWhiteList()
+            {
+                var blob = blobHelper.GetBlobContainer(blobContainer).GetBlockBlobReference(whitelistFileName);
+
+                var ms = new MemoryStream();
+                await blob.DownloadToStreamAsync(ms);
+                ms.Seek(0L, SeekOrigin.Begin);
+
+                var results = new HashSet<int>();
+                string line;
+                using (var reader = new StreamReader(ms))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (string.IsNullOrEmpty(line))
+                        {
+                            continue;
+                        }
+
+                        var ukprn = int.Parse(line);
+                        results.Add(ukprn);
+                    }
+                }
+
+                return results;
+            }
+
+            async Task<Provider> GetExistingCourse(string ukprn, DocumentClient documentClient)
+            {
+                var collectionLink = UriFactory.CreateDocumentCollectionUri(databaseId, coursesCollectionId);
+
+                var query = documentClient
+                    .CreateDocumentQuery<Provider>(collectionLink, new FeedOptions()
+                    {
+                    })
+                    .Where(d => d.UnitedKingdomProviderReferenceNumber == ukprn)
+                    .AsDocumentQuery();
+
+                return (await query.ExecuteNextAsync()).FirstOrDefault();
+            }
         }
+
+
     }
 
     public class ProviderQAStatus
     {
         public int UKPRN { get; set; }
-        public int PassedOverallQAChecks { get; set; }
+        public bool PassedOverallQAChecks { get; set; }
     }
 }
