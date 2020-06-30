b6a19debf859761035f24fa36b1b2f9430c1136f
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2020-06-30 13:34:45 +0100

    added extra fields for logging. (#234)

---
+++ src/Dfc.ProviderPortal.TribalExporter/Models/Dfc/VenueRestorerReference.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dfc.ProviderPortal.TribalExporter.Models.Dfc
+{
+    public class VenueRestorerReference
+    {
+        public Guid? ApprenticeshipId { get; set; }
+        public Guid? CourseRunId { get; set; }
+        public Guid? CourseId { get; set; }
+        public int? ApprenticeshipLocationUKPRN { get; set; }
+        public int UKPRN { get; set; }
+        public string VenueId { get; set; }
+        public int CurrentVenueUKPRN { get; set; }
+        public int? RestoredVenueUKPRN { get; set; }
+        public string CurrentVenueName { get; set; }
+        public string CurrentAddress1 { get; set; }
+        public string CurrentPostcode { get; set; }
+        public string RestoredVenueName { get; set; }
+        public string RestoredAddress1 { get; set; }
+        public string RestoredPostcode { get; set; }
+        public string Message { get; set; }
+        public string Type { get; set; }
+        public bool UKPRNMatched { get; set; }
+    }
+}
